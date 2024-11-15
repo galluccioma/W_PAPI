3c4f5553442372432b758e89d4eea6cf1a51913e
galluccioma <galluccioma@gmail.com>
2024-11-15 17:14:18 +0100

    PUSH REFRACTOR CORRETTO  FUNZIONANTE

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
-//??? Caricamento dei file necessari
+//////////////////// Funzioni del tema Minimal Headless Theme
 require get_template_directory() . '/inc/theme-functions.php';
 require get_template_directory() . '/inc/custom-post-types.php';
 require get_template_directory() . '/inc/api-endpoints.php';
+
+
